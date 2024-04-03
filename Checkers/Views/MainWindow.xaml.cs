db0afe1d67858d0090ca2962538147a5169299ab
Cezar Constăndoiu <93541398+ccaesar26@users.noreply.github.com>
2024-04-03 08:48:00 +0300

    Commit

---
+++ Checkers/Views/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Checkers
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Content = new Views.GamePage();
+        }
+    }
+}
